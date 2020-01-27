d6553ed2350887b223dc18af1f719af35c62b83f
Tom Koch <tzoxster@gmail.com>
2020-01-27 15:50:21 +0200

    added text to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,8 @@ const forecast = (latitude, longitude, callback) => {
         } else {
             const temperature = body.currently.temperature
             const windChance = body.currently.precipProbability
-            callback(undefined, body.daily.data[0].summary + " It is currently " + temperature + " degrees out. There is a " + windChance + "% chance of rain")
+            
+            callback(undefined, body.daily.data[0].summary + " It is currently " + temperature + " degrees out. The high today is " + body.daily.data[0].temperatureHigh + " with a low of " + body.daily.data[0].temperatureLow + " There is a " + windChance + "% chance of rain")
 
         }
     })
